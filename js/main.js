65f86e553f70d6b63e840671c5b3b2bb41f6b57c
Simon Tharby <jinjamail@gmail.com>
2019-06-29 13:27:19 +0100

    tweak delay to improve scroll restore on mobile

--- js/main.js
+++ js/main.js
@@ -91,7 +91,7 @@
         setTimeout(function(){
           window.scrollTo(0, scrollPosn);
           overlay.style.zIndex = '-1';
-        }, 100);
+        }, 10);
       }
     } else {
       overlay.style.zIndex = '-1';
